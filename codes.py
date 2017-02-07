bfe4324b8795f3edb04728ca1fa2ae262f9787d1
AKrikunov <AKrikunov>
2017-02-07 20:11:22 +0300

    Now extracts codes from all files in folder

--- codes.py
+++ codes.py
@@ -1,23 +1,42 @@
 ﻿# -*- coding: utf-8 -*-
-import xml.etree.ElementTree as ET
+from lxml import etree
+import os, sys
+import glob
+import pandas as pd
 
-# Надо будет еще добавить пакетную обработку файлов
-EHR_tree = ET.parse('10_2015.xml')
-root = EHR_tree.getroot()
+folder = sys.argv[1]
+output = sys.argv[2]
+vocab_file = sys.argv[3]
 
-fout = open ("from_xml.txt", "w")
-skipi = 1
-
-for record in root:
-    if skipi > 25: # Пропускаем первые 25 незначащих тегов
-    
-        for field in record.find('Zs0c15'): # В этом поле - диагноз
-            # Хитрая обработка, учитывающая наличие html-тегов
-            fout.write(ET.tostring(field, encoding="utf-8"))
-            fout.write('\n')
+diagnoses = pd.read_csv(vocab_file, sep=';', names  = ['code', 'variants'], encoding="cp1251")
+diagnoses.variants = diagnoses.variants.map(lambda x: [s.strip() for s in x.split("|")])
+ 
+cases = []
+# find all .xml files and iterate them
+parser = etree.XMLParser(ns_clean=True, encoding='cp1251')
+for case_file_name in glob.glob(folder + r"/Прием кардиолога*.xml"):
+    with open(case_file_name, 'r', encoding="cp1251") as case_file:
+        tree = etree.parse(case_file, parser)
+        root = tree.getroot()
         
-            #Таким образом в файле исходные данные для извлечения кодов заболеваний        
-        
-    skipi += 1
+        for case in root.findall(r'OXy'):
+            ID = case.find('Zs0c1').text
+            diag_text = etree.tostring(case.find('Zs0c15'), encoding='cp1251').decode('cp1251')
+            
+            if diag_text == '<Zs0c15>-</Zs0c15>':
+                continue
+            
+            diag_codes = []
+            for row in diagnoses.itertuples(index=False):
+                for v in row.variants:
+                    if v in diag_text:
+                        diag_codes.append(row.code)
+                        break
+                    
+            codes = "|".join(diag_codes)
+            cases.append((ID, codes if codes else None))
+            
+    dd = pd.DataFrame(cases)
+    dd.columns = ["case", "codes"]
 
-fout.close()
+    dd.to_csv(output)
